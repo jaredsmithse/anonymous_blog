904d665877d5760148bea8fd5723a9eb0d58987c
Jared Smith <jared.rit@me.com>
2013-06-27 11:24:44 -0700

    Got the tags to be clickable from the home page and then viewable for all the posts tagged with that tag on another.

--- app/controllers/tags.rb
+++ app/controllers/tags.rb
@@ -1,5 +1,5 @@
-get '/tag/:tag' do
-	@tag = Tag.find_by_title(params[:tag]) 
-	@tagged_posts = tag.posts
+get '/tag/:tag_id' do
+	@tag = Tag.find_by_id(params[:tag_id]) 
+	@tagged_posts = @tag.posts
 	erb :posts_by_tag
 end
