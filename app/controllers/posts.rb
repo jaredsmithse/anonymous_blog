60a82e1e7d961def7fd5500eec0a96298a597e7d
Jared Smith <jared.rit@me.com>
2013-06-27 12:17:15 -0700

    Working on editing a post, right now everything flows fine but it makes a new entry in the database, taking a break until jeffrey comes over.

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -4,15 +4,32 @@ end
 
 post '/create' do 
 	post = Post.create(params[:post])
-	params[:tag][:titles].split(",").each do |tag|
-		post.tags << Tag.find_or_create_by_title(tag.chomp)
-	end
+	make_or_update_tags(params, post)
+	redirect '/'
+end
+
+post '/update' do 
+	post = Post.update_attributes(params[:post])
+	make_or_update_tags(params, post)
 	redirect '/'
 end
 
 get '/post/:post_id' do 
-	@post = Post.find_by_id(params[:post_id])
+	set_current_post
 	erb :post_page
 end
 
+get '/edit/:post_id' do
+	set_current_post
+	erb :edit_post 
+end
 
+def set_current_post
+	@post = Post.find_by_id(params[:post_id])
+end
+
+def make_or_update_tags(args, post)
+	args[:tag][:titles].split(",").each do |tag|
+		post.tags << Tag.find_or_create_by_title(tag.chomp)
+	end
+end
